c2b902621669509ff349aa91d23bf4099f57e579
JONATAS SOLCIA MACHADO <jonatas.smachado1@senacsp.edu.br>
2023-08-18 22:01:28 -0300

    primeirocomponente

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import socialIcons from './assets/socialIcons.png';
+import contactIcons from './assets/contactIcons.png';
 
 export default function Footer(){
     return (
-        <footer className='footerStyle'>
-            <h1>TESTE</h1>
+        <footer className='footer'>
+                <figure><img  className='contactIcons' src={socialIcons} alt="Ícones redes sociais" /></figure>
+                <p className='socialAddress'>@samambaia.foods @samambaia.corp</p>
 
+
+            <div className='center'>
+                <p className='slogan'>PLANTE SEU FUTURO SAMAMBAIA CORP ®</p>
+            </div>
+
+
+                <p className='socialAddress'>samambaia@corp.com (11) 4002-8922</p>
+                <figure><img  className='contactIcons' src={contactIcons} alt="Ícones de contato" /></figure>
         </footer>
     )
 }
---
+++ src/pages/Produtos/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Produtos(){
+    return(
+        <>
+        <h1>TESTE</h1>
+        </>
+       
+    )
+}
+
+export default Produtos;
