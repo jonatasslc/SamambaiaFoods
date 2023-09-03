33da7e78b329d140757025432139dc758a4e1d1d
Ueslei Amaral <uespamaral@gmai.com>
2023-09-02 23:34:11 -0300

    finished page products

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -8,16 +8,16 @@ import imgKibe from "./assets/samamkibe.png";
 export const Products = () => {
   return (
     <div className="container-products">
-      <div className="title-products">
-        <h1>Conheça nossos Produtos </h1>
+      <div className="title-products divTitlep">
+        <h1 className="titlePagep ">Conheça nossos Produtos </h1>
       </div>
 
       <div className="div-cards-products">
         <figure className="image-product">
           <img src={imgBurger} alt="" className="imgproduct" />
         </figure>
-        <h2 className="title-p">
-          SAMAM’BURGER
+        <h2 className="title-h2">
+          <h3 className="titlePagep title-h3">SAMAM’BURGER</h3>
           <p className="description-p">
             Feito à base de proteína de ervilha. Não contém derivados de ovo e
             leite. Fonte de vitamina B12, ferro e fibra alimentar. Contém 14g de
@@ -30,8 +30,8 @@ export const Products = () => {
         <figure className="image-product">
           <img src={imgChicken} alt="" className="imgproduct" />
         </figure>
-        <h2 className="title-p">
-          SAMAM’BURGER
+        <h2 className="title-h2">
+          <h3 className="titlePagep title-h3">SAMAM’BURGER</h3>
           <p className="description-p">
             Feito à base de proteína de ervilha. Não contém derivados de ovo e
             leite. Fonte de vitamina B12, ferro e fibra alimentar. Contém 14g de
@@ -44,8 +44,8 @@ export const Products = () => {
         <figure className="image-product">
           <img src={imgMilk} alt="" className="imgproduct" />
         </figure>
-        <h2 className="title-p">
-          SAMAM’BURGER
+        <h2 className="title-h2">
+          <h3 className="titlePagep title-h3">SAMAM’BURGER</h3>
           <p className="description-p">
             Feito à base de proteína de ervilha. Não contém derivados de ovo e
             leite. Fonte de vitamina B12, ferro e fibra alimentar. Contém 14g de
@@ -58,8 +58,8 @@ export const Products = () => {
         <figure className="image-product">
           <img src={imgKibe} alt="" className="imgproduct" />
         </figure>
-        <h2 className="title-p">
-          SAMAM’BURGER
+        <h2 className="title-h2">
+          <h3 className="titlePagep title-h3">SAMAM’BURGER</h3>
           <p className="description-p">
             Feito à base de proteína de ervilha. Não contém derivados de ovo e
             leite. Fonte de vitamina B12, ferro e fibra alimentar. Contém 14g de
